0d8134b1393fc3e090267ab5e06dce814e520b2b
CodeMaster4711 <cedi27@t-online.de>
2025-11-25 11:34:40 +0100

    feat: Two_Sum Challange

---
+++ Easy/Two_Sum/src/main.rs
@@ -0,0 +1,22 @@
+struct Solution;
+
+impl Solution {
+    pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
+        let mut num_mp = std::collections::HashMap::new();
+        for (i, &num) in nums.iter().enumerate() {
+            let complement = target - num;
+            if let Some(&index) = num_mp.get(&complement) {
+                return vec![index as i32, i as i32];
+            }
+            num_mp.insert(num, i);
+        }
+        vec![]
+    }
+}
+
+fn main() {
+    let nums = vec![2, 7, 11, 15];
+    let target = 9;
+    let result = Solution::two_sum(nums, target);
+    println!("{:?}", result);
+}
