62e7311917cc05f3a213d4a8ef6ba6ba55fc7550
CodeMaster4711 <cedi27@t-online.de>
2025-11-25 10:53:05 +0100

    feat: Contains Duplicate

---
+++ Easy/Contains_Duplicate/src/main.rs
@@ -0,0 +1,20 @@
+struct Solution;
+
+impl Solution {
+    pub fn contains_duplicate(nums: Vec<i32>) -> bool {
+        use std::collections::HashSet;
+        let mut seen = HashSet::new();
+        for num in nums {
+            if !seen.insert(num) {
+                return true;
+            }
+        }
+        false
+    }
+}
+
+fn main() {
+    let nums = vec![1, 2, 3, 1];
+    let result = Solution::contains_duplicate(nums);
+    println!("{}", result);
+}
---
+++ Easy/README.md
@@ -0,0 +1,9 @@
+## 217. Contains Duplicate
+
+### Beschreibung
+
+Gegeben ein Array von ganzen Zahlen, bestimme, ob es Duplikate enthält.
+
+#### Link
+
+[LeetCode Problem](https://leetcode.com/problems/contains-duplicate/description/)
